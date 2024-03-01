cdc4ca843bf10fba35d1a65e283a0a1946089615
Karol Krupa <karo2krupa@gmail.com>
2024-03-01 15:52:15 +0100

    fix

--- tailwind.js
+++ tailwind.js
@@ -1,13 +1,12 @@
 const path = require('path')
 
 export function contentSources(bundleDir = 'vendor/karolkrupa/cms-bundle') {
-
     bundleDir = path.resolve(bundleDir, '.');
     const assetsDir = path.resolve(bundleDir, './assets');
 
     return  [
-        assetsDir + '/src/js/**/*.{js,vue}',
-        assetsDir + '/src/vue/**/*.{js,vue}',
+        path.resolve(__dirname, './src/js') + '/**/*.{js,vue}',
+        path.resolve(__dirname, './src/vue') + '/**/*.{js,vue}',
         bundleDir + "/templates/**/*.twig",
         bundleDir + "/src/**/*.php",
         "node_modules/preline/dist/*.js"
